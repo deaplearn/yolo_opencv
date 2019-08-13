602b2468cf250e91fc7c509411b0a70411ad2c9a
xzming <xzm@umshz.com>
2019-08-13 16:42:19 +0800

    add detect script

---
+++ detect.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+if [[ $1 == "live" ]]; then
+	python3 object_detection_yolo.py 
+elif [[ $1 == *".jpg" ]]; then
+	python3 object_detection_yolo.py --image $1
+elif [[ $1 == *".mp4" ]]; then
+	python3 object_detection_yolo.py --video $1
+else
+	python3 object_detection_yolo.py 
+fi
